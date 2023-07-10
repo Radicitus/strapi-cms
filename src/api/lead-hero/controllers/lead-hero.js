7394ab0fd62f91b96fbaef4372fc3112916a896b
Radicitus <novivolatus@gmail.com>
2023-07-10 13:12:08 -0700

    Adds: - Footer - Hero - Lead Hero - Link - Page And updates schemas

---
+++ src/api/lead-hero/controllers/lead-hero.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lead-hero controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::lead-hero.lead-hero');
